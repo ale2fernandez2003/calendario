735f2d472950159e661d09e5765aab3d5ff62530
Alejandro <ale2fernandez2003@gmail.com>
2023-11-24 13:02:35 +0100

    añadido ano, mes, dia

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -8,26 +8,17 @@
 public class CalendarioBasico
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int dia;
+    private int mes;
+    private int ano;
 
     /**
      * Constructor for objects of class CalendarioBasico
      */
     public CalendarioBasico()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        dia = 1;
+        mes = 1;
+        ano = 1;
     }
 }
