362ea51a5bc4d663241d2233a1bb2e4888df94fc
Alejandro <ale2fernandez2003@gmail.com>
2023-11-24 13:32:38 +0100

    obtener fecha y fijar fecha

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -23,7 +23,20 @@ public class CalendarioBasico
     }
     
     public String obtenerFecha(){
-        return dia + "-" + mes + "-" + ano;
+        String saberDia = dia + "";
+        String saberMes = mes + "";
+        String saberAno = ano + "";
+
+          if(dia < 10){
+            saberDia = "0" + dia;
+        }
+        if(mes < 10){
+            saberMes = "0" + mes;
+        }
+        if(ano < 10){
+            saberAno = "0" + ano;
+        }
+        return saberDia + "-" + saberMes + "-" + saberAno;
     }
     
      public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
